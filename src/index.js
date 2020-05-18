a5a300c8a2144fca0eaa0162698a7f6fe3db6c3a
github username <github email address>
2020-05-18 17:44:13 -0600

    Add redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux' 
+
+const store = createStore(() => {})
+
+function todos(state=[], action) {
+  
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
